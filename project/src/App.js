332d99f365d5dee438c509d272e810ab4adc9510
isha-tripjack <isha.soni@tripjack.com>
2024-10-14 15:13:52 +0530

    project added

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ToDoApp from './Components/ToDoApp'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ToDoApp/>
+      </div>
+    )
+  }
+}
